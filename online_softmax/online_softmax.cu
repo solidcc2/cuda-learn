3605babe3f106b9aec51fe5c2ab28836469f5337
solidcc2 <solidcc@qq.com>
2026-03-19 17:21:27 +0800

    add online softmax gpu & changelog

--- online_softmax/online_softmax.cu
+++ online_softmax/online_softmax.cu
@@ -247,6 +247,105 @@ void softmax_gpu_single_block(float* input, int n, float* output) {
     cudaFree(d_output);
 }
 
+__global__ void _cu_online_softmax_single_block(float* input, int n, float* output) {
+    extern float __shared__ block_buf[];
+    float* maxs = block_buf;
+    float* sums = block_buf + blockDim.x;
+    maxs[threadIdx.x] = -INFINITY;
+    sums[threadIdx.x] = 0.0f;
+    for(int i=threadIdx.x; i<n; i+=blockDim.x) {
+        float in = input[i];
+        float m = max(maxs[threadIdx.x], in);
+        sums[threadIdx.x] = sums[threadIdx.x] * __expf(maxs[threadIdx.x] - m) + __expf(in - m);
+        maxs[threadIdx.x] = m;
+    }
+    __syncthreads();
+    int bound = blockDim.x / 2;
+    for(; bound >= 32; bound >>= 1) {
+        if (threadIdx.x < bound) {
+            float max_ = max(maxs[threadIdx.x], maxs[threadIdx.x + bound]);
+            sums[threadIdx.x] = 
+                sums[threadIdx.x] * __expf(maxs[threadIdx.x] - max_) +
+                sums[threadIdx.x + bound] * __expf(maxs[threadIdx.x + bound] - max_);
+            maxs[threadIdx.x] = max_;
+        }
+        __syncthreads();
+    }
+    __shared__ float s_sum, s_max;
+    if (threadIdx.x < 32) {
+        float max_ = maxs[threadIdx.x];
+        float sum_ = sums[threadIdx.x];
+        float m;
+        float max_n, sum_n;
+        max_n = __shfl_down_sync(0xffffffff, max_, 16);
+        sum_n = __shfl_down_sync(0xffffffff, sum_, 16);
+        m = max(max_, max_n);
+        sum_ = sum_ * __expf(max_ - m) + sum_n * __expf(max_n - m);
+        max_ = m;
+
+
+        max_n = __shfl_down_sync(0xffffffff, max_, 8);
+        sum_n = __shfl_down_sync(0xffffffff, sum_, 8);
+        m = max(max_, max_n);
+        sum_ = sum_ * __expf(max_ - m) + sum_n * __expf(max_n - m);
+        max_ = m;
+
+
+        max_n = __shfl_down_sync(0xffffffff, max_, 4);
+        sum_n = __shfl_down_sync(0xffffffff, sum_, 4);
+        m = max(max_, max_n);
+        sum_ = sum_ * __expf(max_ - m) + sum_n * __expf(max_n - m);
+        max_ = m;
+
+
+        max_n = __shfl_down_sync(0xffffffff, max_, 2);
+        sum_n = __shfl_down_sync(0xffffffff, sum_, 2);
+        m = max(max_, max_n);
+        sum_ = sum_ * __expf(max_ - m) + sum_n * __expf(max_n - m);
+        max_ = m;
+
+
+        max_n = __shfl_down_sync(0xffffffff, max_, 1);
+        sum_n = __shfl_down_sync(0xffffffff, sum_, 1);
+        m = max(max_, max_n);
+        sum_ = sum_ * __expf(max_ - m) + sum_n * __expf(max_n - m);
+        max_ = m;
+
+        if (threadIdx.x == 0) {
+            s_sum = sum_;
+            s_max = max_;
+        }
+    }
+    __syncthreads();
+    for(int i= threadIdx.x; i < n; i+= blockDim.x) {
+        output[i] = __expf(input[i] - s_max) / s_sum;
+    }
+}
+
+void online_softmax_gpu_single_block(float* input, int n, float* output) {
+    float* d_input, *d_output;
+    cudaMalloc(&d_input, sizeof(float)* n);
+    cudaMalloc(&d_output, sizeof(float) * n);
+    cudaMemcpy(d_input, input, sizeof(float)*n, cudaMemcpyHostToDevice);
+
+    cudaEvent_t start, end;
+    cudaEventCreate(&start);
+    cudaEventCreate(&end);
+    cudaEventRecord(start);
+    int block_size = 256;
+    _cu_online_softmax_single_block<<<1, block_size, 2 * block_size * sizeof(float)>>>(d_input, n, d_output);
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+    float elapse;
+    cudaEventElapsedTime(&elapse, start, end);
+    std::cout << "use: " << elapse << "ms" << std::endl;
+    cudaMemcpy(output, d_output, sizeof(float) * n, cudaMemcpyDeviceToHost);
+    
+    cudaFree(d_input);
+    cudaFree(d_output);
+}
+
+
 bool nearly_equal(float a, float b, float rtol = 1e-5f, float atol = 1e-8f) {
     return fabsf(a - b) <= atol + rtol * fmaxf(fabsf(a), fabsf(b));
 }
@@ -272,15 +371,25 @@ int main() {
     online_softmax_cpu(input, input_len, osm_cpu);
     std::cout << "cpu check: " 
         << (check_result(sm_cpu, input_len, osm_cpu) ? "right" : "wrong") << std::endl;
+    free(sm_cpu);
 
     float* sm_gpu = (float*)malloc(sizeof(float)* input_len);
     softmax_gpu(input, input_len, sm_gpu);
     std::cout << "softmax_gpu check: "
         << (check_result(sm_cpu, input_len, sm_gpu) ? "right" : "wrong") << std::endl;
+    free(osm_cpu);
 
-    
     float* sm_gpu_single_block = (float*)malloc(sizeof(float)* input_len);
     softmax_gpu_single_block(input, input_len, sm_gpu_single_block);
     std::cout << "sm_gpu_single_block check: "
         << (check_result(sm_cpu, input_len, sm_gpu_single_block) ? "right" : "wrong") << std::endl;
+    free(sm_gpu_single_block);
+
+    float* osm_gpu_single_block = (float*)malloc(sizeof(float)* input_len);
+    online_softmax_gpu_single_block(input, input_len, sm_gpu_single_block);
+    std::cout << "online_softmax_gpu_single_block check: "
+        << (check_result(sm_cpu, input_len, sm_gpu_single_block) ? "right" : "wrong") << std::endl;
+    free(osm_gpu_single_block);    
+    
+    free(input);
 }
