3aa5a7e39f06b7c8dddaee831b99d6e24acc7e5f
solidcc2 <solidcc@qq.com>
2026-03-20 15:47:34 +0800

    transpose solve bank conflict

--- changelog.md
+++ changelog.md
@@ -1,7 +1,7 @@
 ## changelog
 
 ### 20260320
-transpose朴素gpu版本 & shared_memory tile实现
+transpose朴素gpu版本 & shared_memory tile版本 & bank conflict solve版本
 
 ### 20260319
 完成online softmax gpu版本，未优化。
--- transpose/transpose.cu
+++ transpose/transpose.cu
@@ -88,6 +88,7 @@ void transpose_gpu_v1(const float* input, float* output, int rows, int cols) {
     cudaFree(d_output);
 }
 
+// shared memory tile
 __global__ void _cu_transpose_gpu_v2(const float* input, float* output, int rows, int cols) {
     int x = threadIdx.x + blockDim.x*blockIdx.x;
     int y = threadIdx.y + blockDim.y*blockIdx.y;
@@ -108,6 +109,7 @@ __global__ void _cu_transpose_gpu_v2(const float* input, float* output, int rows
     }
 }
 
+// shared memory tile
 void transpose_gpu_v2(const float* input, float* output, int rows, int cols) {
     float* d_input;
     float* d_output;
@@ -124,6 +126,64 @@ void transpose_gpu_v2(const float* input, float* output, int rows, int cols) {
     dim3 block(BLOCK_X, BLOCK_Y);
     dim3 grid((COLS + BLOCK_X - 1) / BLOCK_X, (ROWS + BLOCK_Y - 1) / BLOCK_Y);
     _cu_transpose_gpu_v2<<<grid, block, sizeof(float) * BLOCK_X * BLOCK_Y>>>(d_input, d_output, ROWS, COLS);
+
+    cudaDeviceSynchronize();
+    cudaMemcpy(output, d_output, sizeof(float) * ROWS * COLS, cudaMemcpyDeviceToHost);
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    float milliseconds = 0.0f;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    std::cout << "GPU time: " << milliseconds << " ms" << std::endl;
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    cudaFree(d_input);
+    cudaFree(d_output);
+}
+
+// bank conflict
+__global__ void _cu_transpose_gpu_v3(const float* input, float* output, int rows, int cols) {
+    int x = threadIdx.x + blockDim.x*blockIdx.x;
+    int y = threadIdx.y + blockDim.y*blockIdx.y;
+    extern __shared__ float tile[];
+    if (x < cols && y < rows) {
+        tile[threadIdx.y * (blockDim.x+1) + threadIdx.x] = input[y * cols + x];
+    }
+    __syncthreads();
+    int o_dim_x = blockDim.y;
+    int o_dim_y = blockDim.x;
+    int tid = blockDim.x * threadIdx.y + threadIdx.x;
+    int o_tid_x = tid % o_dim_x;
+    int o_tid_y = tid / o_dim_x;
+    int o_x = blockIdx.y * blockDim.y + o_tid_x;
+    int o_y = blockIdx.x * blockDim.x + o_tid_y;
+    if (o_x < rows && o_y < cols) {
+        // int pos = o_tid_x * o_dim_y + o_tid_y;
+        // int tile_x = pos % blockDim.x;
+        // int tile_y = pos / blockDim.x;
+        // output[o_y * rows + o_x] = tile[tile_y * (blockDim.x+1) + tile_x];
+        output[o_y * rows + o_x] = tile[o_tid_x * (o_dim_y + 1) + o_tid_y];
+    }
+}
+
+// bank conflict
+void transpose_gpu_v3(const float* input, float* output, int rows, int cols) {
+    float* d_input;
+    float* d_output;
+    cudaMalloc(&d_input, sizeof(float) * ROWS * COLS);
+    cudaMalloc(&d_output, sizeof(float) * ROWS * COLS);
+    cudaMemcpy(d_input, input, sizeof(float) * ROWS * COLS, cudaMemcpyHostToDevice);
+    cudaMemset(d_output, 0, sizeof(float) * ROWS * COLS);
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
+
+    dim3 block(BLOCK_X, BLOCK_Y);
+    dim3 grid((COLS + BLOCK_X - 1) / BLOCK_X, (ROWS + BLOCK_Y - 1) / BLOCK_Y);
+    _cu_transpose_gpu_v3<<<grid, block, sizeof(float) * (BLOCK_X+1) * BLOCK_Y>>>(d_input, d_output, ROWS, COLS);
 
     cudaDeviceSynchronize();
     cudaMemcpy(output, d_output, sizeof(float) * ROWS * COLS, cudaMemcpyDeviceToHost);
@@ -165,6 +225,12 @@ int main() {
     memset(output_gpu, 0, sizeof(float) *ROWS *COLS);
     transpose_gpu_v2(input, output_gpu, ROWS, COLS);
     std::cout << "transpose_gpu_v2: " 
+        << (check_result(output_cpu_ref, output_gpu, COLS, ROWS) ? "right" : "wrong")
+        << std::endl;
+    
+    memset(output_gpu, 0, sizeof(float) *ROWS *COLS);
+    transpose_gpu_v3(input, output_gpu, ROWS, COLS);
+    std::cout << "transpose_gpu_v3: " 
         << (check_result(output_cpu_ref, output_gpu, COLS, ROWS) ? "right" : "wrong")
         << std::endl;
 
